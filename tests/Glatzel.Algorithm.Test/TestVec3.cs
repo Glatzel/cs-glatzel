e1bb183eeaadba82ea174f99d3b880c8c50c0138
Glatzel <893016099@qq.com>
2024-11-04 22:00:43 +0800

    fix

--- tests/Glatzel.Algorithm.Test/TestVec3.cs
+++ tests/Glatzel.Algorithm.Test/TestVec3.cs
@@ -5,96 +5,12 @@ namespace Glatzel.Algorithm.Test;
 public class TestVec3
 {
     [Fact]
-    public void TestConstructor()
-    {
-        {
-            Vec3 v = new();
-            Assert.Equal(0, v.X);
-            Assert.Equal(0, v.Y);
-            Assert.Equal(0, v.Z);
-        }
-        {
-            Vec3 v = new(1, 2, 3);
-            Assert.Equal(1, v.X);
-            Assert.Equal(2, v.Y);
-            Assert.Equal(3, v.Z);
-        }
-        {
-            Vec3 v = new([1, 2, 3]);
-            Assert.Equal(1, v.X);
-            Assert.Equal(2, v.Y);
-            Assert.Equal(3, v.Z);
-        }
-        {
-            Assert.Throws<ArgumentException>(() => new Vec3([1, 2]));
-        }
-    }
-
-    [Fact]
     public void TesOne()
     {
         Vec3 v = Vec3.One;
         Assert.Equal(1, v.X);
         Assert.Equal(1, v.Y);
         Assert.Equal(1, v.Z);
-    }
-
-    [Fact]
-    public void TestUnitX()
-    {
-        Vec3 v = Vec3.UnitX;
-        Assert.Equal(1, v.X);
-        Assert.Equal(0, v.Y);
-        Assert.Equal(0, v.Z);
-    }
-
-    [Fact]
-    public void TestUnitY()
-    {
-        Vec3 v = Vec3.UnitY;
-        Assert.Equal(0, v.X);
-        Assert.Equal(1, v.Y);
-        Assert.Equal(0, v.Z);
-    }
-
-    [Fact]
-    public void TestUnitZ()
-    {
-        Vec3 v = Vec3.UnitZ;
-        Assert.Equal(0, v.X);
-        Assert.Equal(0, v.Y);
-        Assert.Equal(1, v.Z);
-    }
-
-    [Fact]
-    public void TestZero()
-    {
-        Vec3 v = Vec3.Zero;
-        Assert.Equal(0, v.X);
-        Assert.Equal(0, v.Y);
-        Assert.Equal(0, v.Z);
-    }
-
-    [Fact]
-    public void TestIndex()
-    {
-        {
-            Vec3 v = new(1, 2, 3);
-            Assert.Equal(1, v[0]);
-            Assert.Equal(2, v[1]);
-            Assert.Equal(3, v[2]);
-            Assert.Throws<ArgumentException>(() => v[3]);
-        }
-        {
-            Vec3 v = new(1, 1, 1);
-            v[0] = 2;
-            v[1] = 3;
-            v[2] = 4;
-            Assert.Equal(2, v[0]);
-            Assert.Equal(3, v[1]);
-            Assert.Equal(4, v[2]);
-            Assert.Throws<ArgumentException>(() => v[3] = 5);
-        }
     }
 
     [Fact]
@@ -145,6 +61,32 @@ public class TestVec3
             Assert.Equal(2, u.X);
             Assert.Equal(4, u.Y);
             Assert.Equal(6, u.Z);
+        }
+    }
+
+    [Fact]
+    public void TestConstructor()
+    {
+        {
+            Vec3 v = new();
+            Assert.Equal(0, v.X);
+            Assert.Equal(0, v.Y);
+            Assert.Equal(0, v.Z);
+        }
+        {
+            Vec3 v = new(1, 2, 3);
+            Assert.Equal(1, v.X);
+            Assert.Equal(2, v.Y);
+            Assert.Equal(3, v.Z);
+        }
+        {
+            Vec3 v = new([1, 2, 3]);
+            Assert.Equal(1, v.X);
+            Assert.Equal(2, v.Y);
+            Assert.Equal(3, v.Z);
+        }
+        {
+            Assert.Throws<ArgumentException>(() => new Vec3([1, 2]));
         }
     }
 
@@ -220,10 +162,57 @@ public class TestVec3
     }
 
     [Fact]
+    public void TestEquals()
+    {
+        Vec3 u = new(1, 2, 3);
+        Vec3 v = new(3, 4, 5);
+        Vec3 w = new(1, 2, 3);
+        double[] x = [12, 13, 14];
+        object y = 12;
+
+        {
+            Assert.False(u.Equals(v));
+            Assert.True(u.Equals(w));
+            Assert.False(u.Equals(x));
+            Assert.False(u.Equals(y));
+        }
+    }
+
+    [Fact]
+    public void TestGetHashCode()
+    {
+        Vec3 u = new(1, 2, 3);
+        Assert.Equal(HashCode.Combine(1, 2, 3), u.GetHashCode());
+    }
+
+    [Fact]
+    public void TestIndex()
+    {
+        {
+            Vec3 v = new(1, 2, 3);
+            Assert.Equal(1, v[0]);
+            Assert.Equal(2, v[1]);
+            Assert.Equal(3, v[2]);
+            Assert.Throws<ArgumentException>(() => v[3]);
+        }
+        {
+            Vec3 v = new(1, 1, 1);
+            v[0] = 2;
+            v[1] = 3;
+            v[2] = 4;
+            Assert.Equal(2, v[0]);
+            Assert.Equal(3, v[1]);
+            Assert.Equal(4, v[2]);
+            Assert.Throws<ArgumentException>(() => v[3] = 5);
+        }
+    }
+
+    [Fact]
     public void TestLength()
     {
         Vec3 u = new(1, 2, 3);
         Assert.Equal(Sqrt(14), u.Length(), 6);
+        Assert.Equal(Sqrt(14), Vec3.Length(u), 6);
     }
 
     [Fact]
@@ -231,6 +220,7 @@ public class TestVec3
     {
         Vec3 u = new(1, 2, 3);
         Assert.Equal(14, u.Length2(), 6);
+        Assert.Equal(14, Vec3.Length2(u), 6);
     }
 
     [Fact]
@@ -351,17 +341,38 @@ public class TestVec3
     }
 
     [Fact]
-    public void TestEquals()
+    public void TestUnitX()
     {
-        Vec3 u = new(1, 2, 3);
-        Vec3 v = new(3, 4, 5);
-        Vec3 w = new(1, 2, 3);
-        double[] x = [12, 13, 14];
+        Vec3 v = Vec3.UnitX;
+        Assert.Equal(1, v.X);
+        Assert.Equal(0, v.Y);
+        Assert.Equal(0, v.Z);
+    }
 
-        {
-            Assert.False(u.Equals(v));
-            Assert.True(u.Equals(w));
-            Assert.False(u.Equals(x));
-        }
+    [Fact]
+    public void TestUnitY()
+    {
+        Vec3 v = Vec3.UnitY;
+        Assert.Equal(0, v.X);
+        Assert.Equal(1, v.Y);
+        Assert.Equal(0, v.Z);
+    }
+
+    [Fact]
+    public void TestUnitZ()
+    {
+        Vec3 v = Vec3.UnitZ;
+        Assert.Equal(0, v.X);
+        Assert.Equal(0, v.Y);
+        Assert.Equal(1, v.Z);
+    }
+
+    [Fact]
+    public void TestZero()
+    {
+        Vec3 v = Vec3.Zero;
+        Assert.Equal(0, v.X);
+        Assert.Equal(0, v.Y);
+        Assert.Equal(0, v.Z);
     }
 }

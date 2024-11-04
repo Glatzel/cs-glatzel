9a30fe682d2e561a8f2a51d8e7d44d50970e415f
Glatzel <32577755+Glatzel@users.noreply.github.com>
2024-11-04 10:29:10 +0800

    ci: add sln file for ci build (#1)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -4,4 +4,3 @@ Remove-Item ./bin -Recurse -ErrorAction SilentlyContinue
 
 
 msbuild /p:Configuration=Release /p:Platform=x64 /p:DebugSymbols=false /p:DebugType=None /t:Glatzel /restore:true /maxcpucount
-

05c87a75b58a435809bd6463512a7f2e2af28255
Glatzel <893016099@qq.com>
2024-11-04 13:43:50 +0800

    fix

--- tests/Glatzel.Algorithm.Test/TestVec3.cs
+++ tests/Glatzel.Algorithm.Test/TestVec3.cs
@@ -3,8 +3,28 @@ namespace Glatzel.Algorithm.Test;
 public class TestVec3
 {
     [Fact]
-    public void PassingTest()
+    public void TestConstructor()
     {
-        Assert.Equal(2, new Vec3(1, 2, 3).Y);
+        {
+            var v = new Vec3();
+            Assert.Equal(0, v.X);
+            Assert.Equal(0, v.Y);
+            Assert.Equal(0, v.Z);
+        }
+        {
+            var v = new Vec3(1, 2, 3);
+            Assert.Equal(1, v.X);
+            Assert.Equal(2, v.Y);
+            Assert.Equal(3, v.Z);
+        }
+        {
+            var v = new Vec3([1, 2, 3]);
+            Assert.Equal(1, v.X);
+            Assert.Equal(2, v.Y);
+            Assert.Equal(3, v.Z);
+        }
+        {
+            Assert.Throws<ArgumentException>(() => new Vec3([1, 2]));
+        }
     }
 }

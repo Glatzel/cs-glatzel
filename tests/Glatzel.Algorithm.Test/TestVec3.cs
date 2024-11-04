03586f8404c89ee67dfc1b461dfd04e4088fd74f
Glatzel <893016099@qq.com>
2024-11-04 13:29:07 +0800

    fix

--- tests/Glatzel.Algorithm.Test/TestVec3.cs
+++ tests/Glatzel.Algorithm.Test/TestVec3.cs
@@ -7,10 +7,4 @@ public class TestVec3
     {
         Assert.Equal(2, new Vec3(1, 2, 3).Y);
     }
-
-    [Fact]
-    public void FailTest()
-    {
-        Assert.Equal(1, new Vec3(1, 2, 3).Y);
-    }
 }

b968adc99939f961edbfb861cdbb50dfb3718d6d
Glatzel <32577755+Glatzel@users.noreply.github.com>
2024-11-05 21:19:30 +0800

    build: optimize build config (#20)

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -2,5 +2,4 @@ Set-Location $PSScriptRoot
 Set-Location ..
 Remove-Item ./bin -Recurse -ErrorAction SilentlyContinue
 
-
-msbuild /p:Configuration=Release /p:Platform=x64 /p:DebugSymbols=false /p:DebugType=None  /restore:true /maxcpucount
+dotnet build --configuration Release

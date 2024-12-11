943d8c0761279539e39011e5402c81d3529fbe37
Glatzel <32577755+Glatzel@users.noreply.github.com>
2024-12-11 12:32:14 +0800

    chore: modify test scripts (#43)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,3 +1,8 @@
 Set-Location $PSScriptRoot
 Set-Location ..
 dotnet test --logger:junit --collect:"XPlat Code Coverage"
+
+try {dotnet-coverage}
+catch {dotnet tool install --global dotnet-coverage}
+
+dotnet-coverage merge tests/**/TestResults/*/coverage.cobertura.xml  --output coverage.xml --output-format xml

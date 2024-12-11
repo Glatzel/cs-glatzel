faccdb84b448cdb6297e6b8a1c91917f376c09de
Glatzel <893016099@qq.com>
2024-12-11 12:27:42 +0800

    fix

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -5,5 +5,5 @@ dotnet test --logger:junit --collect:"XPlat Code Coverage"
 try {dotnet-coverage}
 catch {dotnet tool install --global dotnet-coverage}
 
-dotnet-coverage merge tests/**/TestResults/*/*.xml  --output coverage.xml --output-format xml
-Remove-Item tests/**/TestResults -Recurse
+dotnet-coverage merge tests/**/TestResults/*/coverage.cobertura.xml  --output coverage.xml --output-format xml
+# Remove-Item tests/**/TestResults -Recurse

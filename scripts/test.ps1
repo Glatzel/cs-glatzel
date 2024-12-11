8a2cbcd991c722935eb054cc929f34678426b54d
Glatzel <893016099@qq.com>
2024-12-11 12:21:57 +0800

    test: modify test workflow

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,3 +1,9 @@
 Set-Location $PSScriptRoot
 Set-Location ..
 dotnet test --logger:junit --collect:"XPlat Code Coverage"
+
+try {dotnet-coverage}
+catch {dotnet tool install --global dotnet-coverage}
+
+dotnet-coverage merge tests/**/TestResults/*/*.xml  --output coverage.xml --output-format xml
+Remove-Item tests/**/TestResults -Recurse

b9beabc6619edc2406cdd2357238b5969da41c66
Glatzel <32577755+Glatzel@users.noreply.github.com>
2024-12-17 10:06:17 +0800

    test: upgrade to xunit v3 (#44)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -2,7 +2,7 @@ Set-Location $PSScriptRoot
 Set-Location ..
 dotnet test --logger:junit --collect:"XPlat Code Coverage"
 
-try {dotnet-coverage}
+try {dotnet-coverage -h}
 catch {dotnet tool install --global dotnet-coverage}
 
 dotnet-coverage merge tests/**/TestResults/*/coverage.cobertura.xml  --output coverage.xml --output-format xml
